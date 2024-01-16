47459da8ae6889834c5f0db299641be8dbacbbab
Justin Winslow <justin@jwin.me>
2024-01-16 13:47:08 -0600

    Initial build

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<section class="view home">
+  <div class="container-wrapper">
+    <section class="container">
+      <h1>Win<span>Media</span></h1>
+      <p>
+        Hi! My name’s Justin Winslow and I’m a general technologist and creative.
+        I’ve been working professionally as a UX designer and software developer
+        since 2007 and spend [way too much of] my free time making and recording music,
+        shooting photos and videos, and doing various design projects.
+      </p>
+
+      <p>
+        Enjoy your stay and if you're interested in collaboration or hiring me,
+        please reach out: <br />
+        <a href="mailTo:justin@jwin.me">justin@jwin.me</a>
+      </p>
+
+      <p>
+        <a href="http://www.jwindesign.com/winslowResume_web.pdf" target="_blank" class="download">
+          Download My Resume
+        </a>
+      </p>
+    </section>
+    <section class="container">
+      <img src="/img/jwin_001.jpg">
+    </section>
+  </div>
+</section>
+
+<style>
+
+@import '../globals.css';
+@import './style.css';
+
+</style>
