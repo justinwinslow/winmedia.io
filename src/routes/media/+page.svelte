11ae73ae7734f716f8d2339db9b4db046ca6db92
Justin Winslow <justin@jwin.me>
2024-01-17 14:34:04 -0600

    Reduce video size

--- src/routes/media/+page.svelte
+++ src/routes/media/+page.svelte
@@ -13,10 +13,10 @@
         <h2>Live in the Studio</h2>
         <p>This is a series of videos of local Austin groups produced, filmed, and recorded by myself and some friends.</p>
         <div class="videos">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/BnUriRNQfRg?si=w0bOhwG38NFmiKYY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/LCv9fguWy1Y?si=VmtGdYfvDPSXCpSv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/ceREKAKV344?si=obpJ9RjLhQvsjnaE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/9et8ueoYgLI?si=k07unLjinTqgxMYF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe width="480" height="270" src="https://www.youtube.com/embed/BnUriRNQfRg?si=w0bOhwG38NFmiKYY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe width="480" height="270" src="https://www.youtube.com/embed/LCv9fguWy1Y?si=VmtGdYfvDPSXCpSv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe width="480" height="270" src="https://www.youtube.com/embed/ceREKAKV344?si=obpJ9RjLhQvsjnaE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe width="480" height="270" src="https://www.youtube.com/embed/9et8ueoYgLI?si=k07unLjinTqgxMYF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
       </li>
     </ul>
